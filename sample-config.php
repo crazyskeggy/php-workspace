3dc4ba88aa797d3672f3358d1ef5b6ef12387c9c
Kieran Peckett <crazyskeggy@gmail.com>
2013-12-27 15:14:06 +0000

    Create config.php - added "sample" to beginning so that it doesn't get replaced on upgrades.

---
+++ sample-config.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * The base configuration of PHP Workspace
+ *
+ * This file contains the following settings:
+ * MySQL Settings
+ * Hash Keys
+ * Default Language
+ *
+ * It is reccomended that you do not store this in a server-visible
+ * directory
+ * 
+ */
+
+
+# MySQL Username
+define("MYSQL_USER", "");
+
+# MySQL Password
+define("MYSQL_PASS", "");
+
+# MySQL Hostname
+define("MYSQL_SERVER", "localhost");
+
+# MySQL Databse Name
+define("MYSQL_DB", "");
+
+# MySQL Table Prefix
+# You can change this setting to have multiple installations of
+# PHP Workspace in the same database.
+# It must end in an underscore, and contain only A-Z, a-z, 0-9 and _
+define("MYSQL_PREFIX", "pw_")
+
+# Default Language - the language must be installed in /includes/langs
+define("DEFAULT_LANG", "en-GB");
+
+/* That's everything! You can now start your installation! */
+
+?>
