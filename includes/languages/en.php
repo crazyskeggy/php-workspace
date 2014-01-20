ceb6bdd50037af21cf038d882e41f695d21bf327
Kieran Peckett <crazyskeggy@gmail.com>
2014-01-20 18:34:29 +0000

    Add initial English translation for main app

---
+++ includes/languages/en.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @ignore
+ */
+
+$en = array(
+	"pw_installed" => "PHP Workspace is installed!",
+	"upgrade" => "Upgrade",
+	"pw_upgrade_available" => "An upgrade for PHP Workspace is available!",
+	"pw_version_number" => sprintf("You have PHP Workspace %1\$s installed.", "0.0.1 alpha")
+);
+
