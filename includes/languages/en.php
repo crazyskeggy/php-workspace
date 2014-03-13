c01fdc61a96ed99b3e31ef4973422b6162eadaab
Kieran Peckett <crazyskeggy@gmail.com>
2014-03-13 21:18:28 +0000

    Update strings for English and add comments describing them

--- includes/languages/en.php
+++ includes/languages/en.php
@@ -5,8 +5,17 @@
 
 $en = array(
 	"pw_installed" => "PHP Workspace is installed!",
-	"upgrade" => "Upgrade",
-	"pw_upgrade_available" => "An upgrade for PHP Workspace is available!",
-	"pw_version_number" => sprintf("You have PHP Workspace %1\$s installed.", "0.0.1 alpha")
+	"upgrade" => "Upgrade", // Used on the update page when an update for PW is available
+	"update" => "Update",   // Used on the update page when an update for addons are available
+	"pw_upgrade_available" => "An upgrade for PHP Workspace is available!", // Used on the update page when a new version is available
+	"pw_version_number" => sprintf('You have PHP Workspace %1$s installed.', pwVersion), // Used on update and about pages
+	"login" => "Login", // Used on Login Button
+	"alpha_version" => "alpha", // context: "Version 1.2.3 alpha"
+	"beta_version" => "beta",   // context: "Version 1.2.3 beta"
+	"label_fileData" => "Upload this:", // On the Upload form
+	"label_folder" => "To this folder:", // On the Upload form
+	"label_desciption" => "With this description:", // On the Upload form 
+	"label_fileName" => "With this file name:", // On the Upload form
+	"submit_button" => "Submit", // On the upload form, amongst others
 );
 
