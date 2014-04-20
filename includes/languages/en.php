8ae8d4cc3575671ec90699c2e382dd233e43d4dd
Kieran Peckett <crazyskeggy@gmail.com>
2014-04-20 16:54:10 +0100

    Update translation strings, including a fix to allow CrowdIn to read the file.

--- includes/languages/en.php
+++ includes/languages/en.php
@@ -4,12 +4,13 @@
  */
 
 $en = array(
-	"pw_installed" => "PHP Workspace is installed!",
+	"pw_installed" => "PHP Workspace is installed!", // Shown when PHP Workspace is updated
 	"upgrade" => "Upgrade", // Used on the update page when an update for PW is available
 	"update" => "Update",   // Used on the update page when an update for addons are available
 	"pw_upgrade_available" => "An upgrade for PHP Workspace is available!", // Used on the update page when a new version is available
-	"pw_version_number" => sprintf('You have PHP Workspace %1$s installed.', pwVersion), // Used on update and about pages
-	"login" => "Login", // Used on Login Button
+	"pw_version_number" => 'You have PHP Workspace %1$s installed.', // Used on update and about pages
+	"login_title" => "Login", // Used in the title for the login page, as well as the tab name
+	"login_button" => "Login", // Used on Login Button
 	"alpha_version" => "alpha", // context: "Version 1.2.3 alpha"
 	"beta_version" => "beta",   // context: "Version 1.2.3 beta"
 	"label_fileData" => "Upload this:", // On the Upload form
@@ -17,5 +18,11 @@ $en = array(
 	"label_desciption" => "With this description:", // On the Upload form 
 	"label_fileName" => "With this file name:", // On the Upload form
 	"submit_button" => "Submit", // On the upload form, amongst others
+	"username" => "Username:", // On login & registration form asking for the user's username - include colon (or language equivalent) at end of string
+	"password" => "Password:", // On login & registration form asking for the user's password - include colon (or language equivalent) at end of string
+	"password_confirm" => "Confirm password:", // On register form asking for a retype of the password - include colon (or language equivalent) at end of string
+	"email" => "Email:", // On register form asking for a the user's email - include colon (or language equivalent) at end of string
+	"email_confirm" => "Confirm email", // On register form asking for a retype of the email - include colon (or language equivalent) at end of string
+	
 );
 
