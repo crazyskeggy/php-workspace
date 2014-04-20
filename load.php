3d64e0236272053c8db24900f6fa6f415149b587
Kieran Peckett <crazyskeggy@gmail.com>
2014-04-20 16:51:06 +0100

    Update PHPDoc comment in load.php
    load.php still has info about includes/checkserver.php, which is now loaded by load-includes.php
    Also adjust the See Also section take them out of being inline and add them to the main DocBlock.

--- load.php
+++ load.php
@@ -4,13 +4,11 @@
  *
  * If the config.php file is missing, then the user will be asked
  * to create it.
+ *
+ * Also loads constants.php to get the constants needed for running.
  * 
- * Also loads includes/checkserver.php to check the server setup for compatibility.
- * {@see includes/checkserver.php}
- * 
- * Also loads includes/constants.php to get the constants needed for running.
- * {@see includes/constants.php}
- * 
+ * @see constants.php
+ * @see includes/load-includes.php"
  * @since 0.0.1
  * 
  */ 
